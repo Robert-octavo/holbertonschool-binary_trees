d31f67703a46299da8642e3e30f5e9c8319de2fe
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-07-26 00:16:28 -0500

    Deletes a tree

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - Write a function that deletes an entire binary tree
+ * @tree: pointer to the parent node
+ */
+
+void binary_tree_delete(binary_tree_t *tree)
+{
+    /*TODO: function that deletes an entire binary tree*/
+	if (tree == NULL)
+		return;
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
+	free(tree);
+}
