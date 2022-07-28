a842ed5c5fdab6cba9d48c1df5c2cf560d3e11bf
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-07-28 17:24:01 -0500

    test function binary tree is perfect

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,42 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_size -  Write a function that measures the size of a binary tree
+ * @tree: pointer to the root node of the tree to measure the height.
+ * Return: If tree is NULL, your function must return 0
+ */
+
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+    /**
+	* TODO: Write a function that measures the size of a binary tree
+	*/
+
+	size_t sum = 1;
+
+	if (tree == NULL)
+		return (0);
+
+	sum = sum + binary_tree_size(tree->left) + binary_tree_size(tree->right);
+	return (sum);
+}
+
+/**
+ * binary_tree_is_full - Write a function that checks if a binary tree is perfect
+ * @tree: pointer to the root node of the tree to measure the height.
+ * Return: If tree is NULL, your function must return 0
+ */
+
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+    /**
+	* TODO: function that checks if a binary tree is perfect
+	*/
+
+	if (tree == NULL)
+		return (0);
+
+	return (binary_tree_size(tree->left) == binary_tree_size(tree->right) ? 1 : 0);
+
+	/*return (0);*/
+}
