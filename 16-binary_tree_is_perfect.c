7781265a09e523705be7ebf5668dab13900fd2dc
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-07-28 17:28:51 -0500

    correct betty

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -22,7 +22,8 @@ size_t binary_tree_size(const binary_tree_t *tree)
 }
 
 /**
- * binary_tree_is_full - Write a function that checks if a binary tree is perfect
+ * binary_tree_is_perfect - Write a function that checks if a binary
+ * tree is perfect
  * @tree: pointer to the root node of the tree to measure the height.
  * Return: If tree is NULL, your function must return 0
  */
@@ -32,11 +33,15 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
     /**
 	* TODO: function that checks if a binary tree is perfect
 	*/
+	int left, right;
 
 	if (tree == NULL)
 		return (0);
 
-	return (binary_tree_size(tree->left) == binary_tree_size(tree->right) ? 1 : 0);
+	left = binary_tree_size(tree->left);
+	right = binary_tree_size(tree->right);
+
+	return (left == right ? 1 : 0);
 
 	/*return (0);*/
 }
